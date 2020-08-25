4fd021c8de08aa62c64ff15b1fb24043beffd4f5
Johannes Filter <hi@jfilter.de>
2020-08-25 18:52:25 +0200

    format with black

--- scraper.py
+++ scraper.py
@@ -12,16 +12,18 @@ import scraperwiki
 
 DEBUG = False
 
+
 def split_date_county(t):
     t = normalize_whitespace(t, no_line_breaks=True)
-    if '|' in t:
-        date, county = t.split('|')
+    if "|" in t:
+        date, county = t.split("|")
     else:
         date = t
-        county = 'Landkreis unbekannt'
-    date = date.replace('Vorfall vom', '')
-    date = parse(date, languages=['de'])
+        county = "Landkreis unbekannt"
+    date = date.replace("Vorfall vom", "")
+    date = parse(date, languages=["de"])
     return date, county.strip()
+
 
 def process_page(doc):
     for entry in doc.xpath("//article[@class='post-model']"):
@@ -29,29 +31,33 @@ def process_page(doc):
         date, county = split_date_county(date_county)
 
         # headline contains links to details page
-        location = entry.xpath('.//h2//text()')[0]
+        location = entry.xpath(".//h2//text()")[0]
         uri = "https://www.raa-sachsen.de" + entry.xpath(".//h2/a/@href")[0]
 
         if DEBUG:
             print(uri)
 
         # getting main text, consider special cases
-        text_list = entry.xpath(".//div[contains(@class, 'content-element summary')]//p/text()")
+        text_list = entry.xpath(
+            ".//div[contains(@class, 'content-element summary')]//p/text()"
+        )
         if len(text_list) == 0:
-            text_list = entry.xpath('.//p/text()')
+            text_list = entry.xpath(".//p/text()")
         if len(text_list) == 0:
-            text_list = entry.xpath(".//div[contains(@class, 'content-element')]/text()")
+            text_list = entry.xpath(
+                ".//div[contains(@class, 'content-element')]/text()"
+            )
 
         text_list = [normalize_whitespace(t, no_line_breaks=True) for t in text_list]
         text_list = [t for t in text_list if len(t) > 0]
 
         # skip if really no text
         if len(text_list) == 0:
-            print('no data for: ', uri)
+            print("no data for: ", uri)
             continue
 
-        if text_list[-1].startswith('Quelle:'):
-            sources = text_list[-1].replace('Quelle:', '')
+        if text_list[-1].startswith("Quelle:"):
+            sources = text_list[-1].replace("Quelle:", "")
             text_list = text_list[:-1]
         else:
             # old format
@@ -63,7 +69,7 @@ def process_page(doc):
                 text_list = text_list[:-1]
 
         if not sources is None:
-            sources = [t.strip() for t in sources.split(',')]
+            sources = [t.strip() for t in sources.split(",")]
 
         title = None
         if len(text_list) > 1 and len(text_list[0]) * 2 < len(text_list[1]):
@@ -84,11 +90,10 @@ def process_page(doc):
                 "iso3166_2": "DE-SN",
                 "url": uri,
                 "identifier": uri,
-                "aggregator": "RAA Sachsen"
+                "aggregator": "RAA Sachsen",
             },
             table_name="incidents",
         )
-
 
         if not sources is None:
             for s in sources:
@@ -103,7 +108,7 @@ base_url = "https://www.raa-sachsen.de/support/chronik?page=%s"
 
 # get count of pages
 html = scraperwiki.scrape(base_url % 1)
-last_page = re.findall(r'Seite 1 von (\d\d\d)', str(html))[0]
+last_page = re.findall(r"Seite 1 von (\d\d\d)", str(html))[0]
 
 urls = [base_url % i for i in range(1, int(last_page) + 1)]
 
